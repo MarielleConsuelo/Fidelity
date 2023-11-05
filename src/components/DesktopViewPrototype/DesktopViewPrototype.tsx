71e5cdc505c3e005205de60949d2c09f0d32c306
Marielle Consuelo <148992073+MarielleConsuelo@users.noreply.github.com>
2023-11-05 12:11:52 -0600

    Initial commit

---
+++ src/components/DesktopViewPrototype/DesktopViewPrototype.tsx
@@ -0,0 +1,353 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import resets from '../_resets.module.css';
+import classes from './DesktopViewPrototype.module.css';
+
+interface Props {
+  className?: string;
+}
+/* @figmaId 7:2 */
+export const DesktopViewPrototype: FC<Props> = memo(function DesktopViewPrototype(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <div className={classes.desktopPage}>
+        <div className={classes.rectangle53}></div>
+        <div className={classes.finance}>Finance</div>
+        <div className={classes.trackYourFinancesWhileLearning}>
+          Track your finances while learning better spending and saving habits.
+        </div>
+        <div className={classes.retirementPlans}>Retirement Plans</div>
+        <div className={classes.trackYourGoalsWhetherYouReSavi}>
+          Track your goals whether you’re saving or already retired. See how small changes could improve your outlook.
+        </div>
+        <div className={classes.investmentPlans}>Investment Plans</div>
+        <div className={classes.discoverInvestmentOpportunitie}>
+          Discover investment opportunities while tracking your current growth.
+        </div>
+        <div className={classes.rectangle51}></div>
+        <div className={classes.learning}>Learning</div>
+        <div className={classes.developYourAbilitiesInUndersta}>
+          Develop your abilities in understanding your finances in a fun and intuitive way.
+        </div>
+        <div className={classes.rectangle512}></div>
+        <div className={classes.image14}></div>
+        <div className={classes.frame1}></div>
+        <div className={classes.getOurNewApp}>Get Our New App!</div>
+        <div className={classes.rectangle513}></div>
+        <div className={classes.image15}></div>
+        <div className={classes.image17}></div>
+        <div className={classes.image19}></div>
+        <div className={classes.rectangle45}></div>
+        <div className={classes.rectangle44}></div>
+        <div className={classes.managedAccounts}>Managed Accounts</div>
+        <div className={classes.rectangle50}></div>
+        <div className={classes.portfolioAdvisoryServicesAccou}>Portfolio Advisory Services Accounts</div>
+        <div className={classes.rectangle514}></div>
+        <div className={classes.learnMore}>
+          <a
+            href='https://www.fidelity.com/wealth-management/tax-smart-investing-planning'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.youCanWorkWithAFidelityAdvisor}>
+          You can work with a Fidelity advisor, who can explain the benefits of applying tax-smart investing techniques
+          in your managed portfolio throughout the year. Because it&#39;s not what you earn, it&#39;s what you keep.
+        </div>
+        <div className={classes.rectangle48}></div>
+        <div className={classes.fidelityManagedFidFolios}>Fidelity Managed FidFolios </div>
+        <div className={classes.youKnowHowYouWantToInvestNowPu}>
+          You know how you want to invest. Now put the pros to work for you. We’ll build and manage a portfolio of
+          hundreds of stocks, looking for tax savings along the way.
+        </div>
+        <div className={classes.rectangle49}></div>
+        <div className={classes.learnMore2}>
+          <a
+            href='https://digital.fidelity.com/prgw/digital/msw/overview/a'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock2}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.rectangle46}></div>
+        <div className={classes.fidelityGo}>Fidelity Go</div>
+        <div className={classes.rectangle47}></div>
+        <div className={classes.learnMore3}>
+          <a
+            href='https://www.fidelity.com/managed-accounts/fidelity-go/overview'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock3}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.useOurRoboAdvisorsToHelpYouHan}>
+          Use our robo-advisors to help you handle your investing needs.
+        </div>
+        <div className={classes.rectangle37}></div>
+        <div className={classes.rectangle40}></div>
+        <div className={classes._529PlansAreFlexibleTaxAdvantag}>
+          529 plans are flexible, tax-advantaged accounts designed specifically for education savings. Funds can be used
+          for qualified education expenses at schools nationwide.
+        </div>
+        <div className={classes._592Account}>
+          <p className={classes.labelWrapper}>
+            <span className={classes.label}>592</span>
+            <span className={classes.label2}> Account</span>
+          </p>
+        </div>
+        <div className={classes.rectangle41}></div>
+        <div className={classes.learnMore4}>
+          <a
+            href='https://www.fidelity.com/retirement-ira/roth-ira'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock4}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.rectangle39}></div>
+        <div className={classes.fidelityYouthAccount}>Fidelity Youth Account</div>
+        <div className={classes.rectangle42}></div>
+        <div className={classes.learnMore5}>
+          <a
+            href='https://www.fidelity.com/go/youth-account/overview'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock5}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.aFreeAppThatGivesTeensAges1317}>
+          A free app that gives teens ages 13–17 the power to make their own money moves—while letting parents stay
+          connected
+        </div>
+        <div className={classes.rectangle36}></div>
+        <div className={classes.savingInvestingForAChild}>Saving &amp; Investing for a Child</div>
+        <div className={classes.rectangle29}></div>
+        <div className={classes.rectangle30}></div>
+        <div className={classes.cashManagementAccount}>Cash Management Account</div>
+        <div className={classes.rectangle35}></div>
+        <div className={classes.learnMore6}>
+          <a
+            href='https://www.fidelity.com/spend-save/fidelity-cash-management-account/overview'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock6}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.separatingYourWorkAndPersonalM}>
+          Separating your work and personal money lets you simplify your cash flow so you can take control of your
+          finances. If you&#39;re not incorporated, a Fidelity® Cash Management Account for your work finances can help.
+        </div>
+        <div className={classes.rectangle31}></div>
+        <div className={classes.smallBusinessHSA}>Small Business HSA</div>
+        <div className={classes.healthSavingsAccountsForSmallB}>
+          Health savings accounts for small businesses and their employees
+        </div>
+        <div className={classes.rectangle34}></div>
+        <div className={classes.learnMore7}>
+          <a
+            href='https://www.fidelity.com/go/hsa/small-business-hsa?ccSource=se_hsa_mp_open_acct_pg'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock7}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.rectangle32}></div>
+        <div className={classes.simplifiedEmployeePensionPlans}>
+          Simplified Employee Pension Plans (SEP IRAs) help self-employed individuals and small-business owners get
+          access to a tax-deferred benefit when saving for retirement.
+        </div>
+        <div className={classes.sEPIRA}>SEP IRA</div>
+        <div className={classes.rectangle33}></div>
+        <div className={classes.learnMore8}>
+          <a
+            href='https://www.fidelity.com/retirement-ira/small-business/sep-ira?ccSource=se_sep_mp_open_acct_pg'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock8}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.rectangle28}></div>
+        <div className={classes.selfEmployedOrGrowingABusiness}>Self-Employed or Growing a Business</div>
+        <div className={classes.rectangle20}></div>
+        <div className={classes.rectangle21}></div>
+        <div className={classes.rectangle23}></div>
+        <div className={classes.fidelityBloom}>Fidelity Bloom</div>
+        <div className={classes.rectangle24}></div>
+        <div className={classes.meetTheAccountThatHelpsMeetYou}>
+          <div className={classes.textBlock9}>
+            Meet the account that helps meet your banking needs and more. With no account fees or minimums to open an
+            account, and competitive rates, it&#39;s never been easier to spend and save smarter.
+          </div>
+          <div className={classes.textBlock10}>
+            <p></p>
+          </div>
+        </div>
+        <div className={classes.fidelityCashManagementAccount}>Fidelity Cash Management Account</div>
+        <div className={classes.rectangle25}></div>
+        <div className={classes.learnMore9}>
+          <a
+            href='https://www.fidelity.com/spend-save/fidelity-cash-management-account/overview'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock11}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.fidelityBloomIsAFreeFinancialA}>
+          Fidelity Bloom® is a free financial app. We use psychology to help gently improve your saving and spending
+          habits. No drastic lifestyle changes required.
+        </div>
+        <div className={classes.rectangle26}></div>
+        <div className={classes.learnMore10}>
+          <a
+            href='https://www.fidelity.com/mobile/bloom'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock12}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.spendingAndSaving}>Spending and Saving</div>
+        <div className={classes.rectangle212}></div>
+        <div className={classes.rectangle8}></div>
+        <div className={classes.thisFullFeaturedLowCostBrokera}>
+          This full-featured, low-cost brokerage account can meet your needs as you grow as an investor.
+        </div>
+        <div className={classes.brokerageAccount}>Brokerage Account</div>
+        <div className={classes.rectangle11}></div>
+        <div className={classes.learnMore11}>
+          <a
+            href='https://www.fidelity.com/trading/the-fidelity-account'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock13}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.rectangle9}></div>
+        <div className={classes.fidelityCrypto}>Fidelity Crypto</div>
+        <div className={classes.tapIntoTheNewCryptoFrontierWit}>
+          Tap into the new crypto frontier with a trusted leader. Introducing Fidelity Crypto®, a breakthrough way to
+          trade bitcoin and ethereum in the same app where you trade stocks.
+        </div>
+        <div className={classes.rectangle12}></div>
+        <div className={classes.learnMore12}>
+          <a
+            href='https://www.fidelity.com/crypto/trading'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock14}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.rectangle16}></div>
+        <div className={classes.trading}>Trading</div>
+        <div className={classes.rectangle15}></div>
+        <div className={classes.savingForRetirement}>Saving For Retirement</div>
+        <div className={classes.rectangle7}></div>
+        <div className={classes.rectangle10}></div>
+        <div className={classes.rolloverIRA}>Rollover IRA</div>
+        <div className={classes.rectangle13}></div>
+        <div className={classes.learnMore13}>
+          <a
+            href='https://www.fidelity.com/retirement-ira/401k-rollover-ira'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock15}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.withABroadRangeOfInvestmentOpt}>
+          With a broad range of investment options, a Fidelity rollover IRA gives you the choice to invest on your own
+          or have Fidelity invest for you. With both, you&#39;ll enjoy the exceptional service, planning and guidance
+          that Fidelity is known for.
+        </div>
+        <div className={classes.rectangle92}></div>
+        <div className={classes.traditionalIRA}>Traditional IRA</div>
+        <div className={classes.withFidelityYouHaveABroadRange}>
+          With Fidelity, you have a broad range of investment options, including options to have us manage your money
+          for you. You&#39;ll get exceptional service as well as planning and guidance support.
+        </div>
+        <div className={classes.rectangle122}></div>
+        <div className={classes.learnMore14}>
+          <a
+            href='https://www.fidelity.com/retirement-ira/traditional-ira'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock16}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.rectangle82}></div>
+        <div className={classes.rothIRA}>Roth IRA</div>
+        <div className={classes.rectangle112}></div>
+        <div className={classes.learnMore15}>
+          <a
+            href='https://www.fidelity.com/retirement-ira/roth-ira'
+            target='_blank'
+            rel='noreferrer'
+            className={classes.textBlock17}
+          >
+            Learn More.
+          </a>
+        </div>
+        <div className={classes.theOpportunityToGrowYourRetire}>
+          The opportunity to grow your retirement savings―and withdraw money when needed. That&#39;s the power and
+          flexibility a Roth IRA can provide.
+        </div>
+        <div className={classes.rectangle6}></div>
+        <div className={classes.youVeGotGoalsWeVeGotSolutions}>You’ve got goals. We’ve got solutions.</div>
+        <div className={classes.frame4}></div>
+        <div className={classes.evenWhenYouReEmployedWeCanHelp}>
+          <div className={classes.textBlock18}>
+            Even when you’re employed, we can help you feel more prepared to manage your finances. We offer many
+            different services to ensure that you are ready financially.
+          </div>
+          <div className={classes.textBlock19}>
+            <p></p>
+          </div>
+        </div>
+        <div className={classes.frame3}></div>
+        <div className={classes.asYouGetOlderItBecomesMoreAppa}>
+          As you get older it becomes more apparent that money is something that will always be of importance,
+          especially when looking for a home or car.
+        </div>
+        <div className={classes.whyYouShouldStartEarly}>Why you should start early.</div>
+        <div className={classes.image1}></div>
+        <div className={classes.rectangle14}></div>
+        <div className={classes.financialAdvising}>Financial Advising</div>
+        <div className={classes.rectangle5}></div>
+        <div className={classes.yourFutureStartsHere}>Your Future Starts Here.</div>
+        <div className={classes.rectangle1}></div>
+        <div className={classes.customerProfileOpenAnAccountVi}>
+          Customer | Profile | Open An Account | Virtual Assistant | LOG IN
+        </div>
+        <div className={classes.fidelity}>Fidelity.</div>
+      </div>
+    </div>
+  );
+});
